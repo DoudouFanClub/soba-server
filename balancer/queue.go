90904033420301c22e243000129d236ba975579f
omnomburp <89351913+omnomburp@users.noreply.github.com>
2024-06-03 22:17:50 +0800

    Update queue.go
    Implementation of a doubly LinkedList that forces type checking, Queue implementation based on this.

--- balancer/queue.go
+++ balancer/queue.go
@@ -1,28 +1,62 @@
 package balancer
 
-import (
-	"container/list"
-)
-
-type Queue struct {
-	queue *list.List
-	size  int
+type node[T any] struct {
+	next *node[T]
+	prev *node[T]
+	val  T
 }
 
-func NewQueue() Queue {
-	return Queue{queue: list.New(), size: 0}
+type LinkedList[T any] struct {
+	head *node[T]
+	tail *node[T]
+	len  int
 }
 
-func (q *Queue) Add(t any) {
-	q.queue.PushBack(t)
-	q.size += 1
+type Queue[T any] struct {
+	queue *LinkedList[T]
 }
 
-func (q *Queue) Pop() {
-	q.queue.Remove(q.queue.Front())
-	q.size -= 1
+func CreateNode[T any]() *node[T] {
+	var zero T
+	return &node[T]{next: nil, prev: nil, val: zero}
 }
 
-func (q *Queue) Empty() bool {
-	return q.size != 0
+func NewLL[T any]() *LinkedList[T] {
+	root := CreateNode[T]()
+	return &LinkedList[T]{head: root, tail: root}
+}
+
+func (l *LinkedList[T]) Add(val T) {
+	l.tail.val = val
+	l.tail.next = &node[T]{}
+	l.tail = l.tail.next
+	l.len += 1
+}
+
+func (l *LinkedList[T]) RemoveFront() T {
+	if l.len > 0 {
+		temp := l.head.val
+		l.head = l.head.next
+		l.len -= 1
+		return temp
+	}
+	var zero T
+	return zero
+}
+
+func NewQueue[T any]() Queue[T] {
+	ll := NewLL[T]()
+	return Queue[T]{queue: ll}
+}
+
+func (q *Queue[T]) Add(val T) {
+	q.queue.Add(val)
+}
+
+func (q *Queue[T]) Pop() T {
+	return q.queue.RemoveFront()
+}
+
+func (q *Queue[T]) Empty() bool {
+	return q.queue.len != 0
 }
