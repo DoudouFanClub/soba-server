f356ebe96349e51b7603145a6df755391746a61e
omnomburp <89351913+omnomburp@users.noreply.github.com>
2024-06-03 22:35:56 +0800

    Update balancer.go
    update: some implementation for the balancer done, need to research more into go channels to bubble events upstream

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -1,1 +1,24 @@
 package balancer
+
+import (
+	"llm_server/socket"
+)
+
+type Balancer struct {
+	Availables Queue[socket.Endpoint]
+}
+
+func CreateBalancer() Balancer {
+	q := NewQueue[socket.Endpoint]()
+	return Balancer{Availables: q}
+}
+
+func (b *Balancer) CanSend() bool {
+	return b.Availables.Empty()
+}
+
+func (b *Balancer) Send(msg []byte, buf *[]byte) {
+	sender := b.Availables.Pop()
+	sender.SendMessage(msg, buf)
+	b.Availables.Add(sender)
+}
