a1bc2757b383513785aa70caadefcbd976612898
omnomburp <89351913+omnomburp@users.noreply.github.com>
2024-06-07 21:20:08 +0800

    Balancer redesign
    update: instead of passing a message buffer in to continuously read from, pass a reference of http.ResponseWriter for the socket to write the http response

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -2,6 +2,7 @@ package balancer
 
 import (
 	"llm_server/socket"
+	"net/http"
 )
 
 type Balancer struct {
@@ -17,8 +18,8 @@ func (b *Balancer) Available() bool {
 	return b.availables.Empty()
 }
 
-func (b *Balancer) Send(msg []byte, buf *[]byte) {
+func (b *Balancer) Send(msg []byte, w *http.ResponseWriter) {
 	sender := b.availables.Pop()
-	sender.SendMessage(msg, buf)
+	sender.SendMessage(msg, w)
 	b.availables.Add(sender)
 }
